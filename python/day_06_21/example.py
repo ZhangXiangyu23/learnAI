030ba53fe2e3e90a4d1e54cf8758ae503d6cfcc7
ZhangXiangyu <884188766@qq.com>
2022-06-22 09:18:33 +0800

    对校验的修改

--- python/day_06_21/example.py
+++ python/day_06_21/example.py
@@ -19,7 +19,7 @@ window.geometry("1000x500")
 
 # 建立一个画布
 canvas = tk.Canvas(window, width=1000, height=200, bg="blue")
-# 防止画布
+# 放置画布
 canvas.pack()
 image_file = tk.PhotoImage(file="img_1.png")
 # 将图片文件放到画布中
@@ -51,30 +51,35 @@ var_register_password = tk.StringVar()
 var_confirm_password = tk.StringVar()
 
 
+# 注册处理
 def do_register():
     register_username_v = var_register_username.get()
     register_password_v = var_register_password.get()
     confirm_password_v = var_confirm_password.get()
-    if register_username_v == "":
-        messagebox.showwarning(title="注册失败", message="用户名不能为空!")
-    if register_password_v == "":
-        messagebox.showwarning(title="注册失败", message="密码不能为空!")
-
-
+    db_have = False
     for item in date_base:
         if item.get("username") == register_username_v:
-            messagebox.showwarning(title="注册失败", message="此用户名已经被注册！")
-
-
-    if register_password_v != confirm_password_v:
+            db_have = True
+            break
+    # 格式校验，并进行提示
+    if register_username_v == "":
+        messagebox.showwarning(title="注册失败", message="用户名不能为空!")
+        return
+    elif register_password_v == "":
+        messagebox.showwarning(title="注册失败", message="密码不能为空!")
+        return
+    elif date_base == True:
+        messagebox.showwarning(title="注册失败",  message="该用户名已存在")
+    elif register_password_v != confirm_password_v:
         messagebox.showwarning(title="两次密码不一致", message="您两次输入的密码不一致啊!")
 
+    # 格式校验成功，进行注册
     if register_username_v != "" and register_password_v != "" and confirm_password_v != "" and register_password_v == confirm_password_v:
         date_base.append({"username": register_username_v, "password": register_password_v})
         messagebox.showinfo(title="注册成功", message=f"恭喜用户:{register_username_v}注册成功")
 
 
-# 注册工作
+# 注册页面
 def to_register():
     # 子窗口
     sub_window = tk.Toplevel(window)
@@ -83,59 +88,63 @@ def to_register():
 
     # 用户名标签
     username_l = tk.Label(sub_window, text="用户名", font="楷体")
-    username_l.place(x=201, y=51)
+    username_l.place(x=271, y=151)
 
     # 密码标签
     password_l = tk.Label(sub_window, text="密码", font="楷体", )
-    password_l.place(x=201, y=121)
+    password_l.place(x=271, y=201)
 
-    # 密码标签
+    # 确认密码标签
     confirm_password_l = tk.Label(sub_window, text="确认密码", font="楷体", )
-    confirm_password_l.place(x=201, y=191)
+    confirm_password_l.place(x=271, y=251)
 
     # 用户名输入框
     username_entry = tk.Entry(sub_window, width=30, textvariable=var_register_username)
-    username_entry.place(x=351, y=51)
+    username_entry.place(x=351, y=151)
 
     # 密码输入框
     password_entry = tk.Entry(sub_window, width=30, textvariable=var_register_password, show="*")
-    password_entry.place(x=351, y=121)
+    password_entry.place(x=351, y=201)
 
     # 确认密码输入框
     confirm_password_entry = tk.Entry(sub_window, width=30, textvariable=var_confirm_password, show="*")
-    confirm_password_entry.place(x=351, y=191)
+    confirm_password_entry.place(x=351, y=251)
 
     # 注册按钮
     register_button = tk.Button(sub_window, text="注册", font="楷体", command=do_register)
-    register_button.place(x=351, y=251)
+    register_button.place(x=401, y=301)
 
 
 # 登录工作
 def login_job():
     username_value = var_username.get()
     password_value = var_password.get()
-    count = 0
-    # 先看看有没有这个用户
-    for j in date_base:
-        count += 1
-        # 存在的话，跳出循环
-        if username_value == j.get("username"):
+    db_have = False
+    # 先看是否存在该用户
+    for item in date_base:
+        if username_value == item.get("username"):
+            db_have = True
             break
-        # 不存在时
-        if username_value != j.get("username") and count == len(date_base):
-            result = messagebox.askokcancel(title="该用户不存在", message="该用户不存在，亲，您是否马上去注册？")
-            if result == True:
-                to_register()
 
 
-    for i in date_base:
-        if username_value == i.get("username") and password_value == i.get("password"):
-            messagebox.showinfo(title="登录成功", message=f"{username_value}，恭喜您登录成功!")
-            break
-        elif username_value == i.get("username") and password_value != i.get("password"):
-            messagebox.showerror(title="登录失败", message="密码错误")
 
-
+    # 各种校验
+    if username_value == "":
+        messagebox.showwarning(title="用户名不能为空", message="亲，用户名不能为空")
+    elif password_value == "":
+        messagebox.showwarning(title="密码不能为空", message="亲，密码不能为空")
+    elif db_have == False:
+        result = messagebox.askokcancel(title="该用户不存在", message="该用户不存在，亲，您是否马上去注册？")
+        if result == True:
+            to_register()
+    else:
+        # 用户名在数据库中存在
+        for i in date_base:
+            if username_value == i.get("username") and password_value == i.get("password"):
+                messagebox.showinfo(title="登录成功", message=f"{username_value}，恭喜您登录成功!")
+                break
+            elif username_value == i.get("username") and password_value != i.get("password"):
+                messagebox.showerror(title="登录失败", message="密码错误")
 
 
 # 登录按钮
@@ -145,10 +154,6 @@ login_button.place(x=401, y=351)
 # 注册按钮
 register_button = tk.Button(window, text="注册", font="楷体", command=to_register)
 register_button.place(x=521, y=351)
-
-
-
-
 
 # 将窗口循环起来
 window.mainloop()
